c8d44b9b5af4906ef0fbc268275d9a02dc99c964
Forrest McDougal <forrestmcdougal@gmail.com>
2019-06-08 23:40:09 -0400

    start of search, CH2

---
+++ CH2_search_problems/generic_search.py
@@ -0,0 +1,65 @@
+from __future__ import annotations
+from typing import (
+    TypeVar,
+    Iterable,
+    Sequence,
+    Generic,
+    List,
+    Callable,
+    Set,
+    Deque,
+    Dict,
+    Any,
+    Optional,
+)
+from typing_extensions import Protocol
+from heapq import heappush, heappop
+
+T = TypeVar("T")
+
+
+def linear_contains(iterable: Iterable[T], key: T) -> bool:
+    for item in iterable:
+        if item == key:
+            return True
+    return False
+
+
+C = TypeVar("C", bound="Comparable")
+
+
+class Comparable(Protocol):
+    def __eq__(self, other: Any) -> bool:
+        ...
+
+    def __lt__(self: C, other: C) -> bool:
+        ...
+
+    def __gt__(self: C, other: C) -> bool:
+        return (not self < other) and self != other
+
+    def __le__(self: C, other: C) -> bool:
+        return self < other or self == other
+
+    def __ge__(self: C, other: C) -> bool:
+        return not self < other
+
+
+def binary_contains(sequence: Sequence[C], key: C) -> bool:
+    low: int = 0
+    high: int = len(sequence) - 1
+    while low <= high:  # while there is still search space
+        mid: int = (low + high) // 2
+        if sequence[mid] < key:
+            low = mid + 1
+        elif sequence[mid] > key:
+            high = mid - 1
+        else:
+            return True
+    return False
+
+
+if __name__ == "__main__":
+    print(linear_contains([1, 5, 15, 15, 15, 15, 20], 5))  # True
+    print(binary_contains(["a", "d", "e", "f", "z"], "f"))  # True
+    print(binary_contains(["john", "mark", "ronald", "sarah"], "Sheila"))  # False
